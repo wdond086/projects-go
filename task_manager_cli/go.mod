4fd93c5d5514f53cdfce772f0a487bc964630f55
Wilfried Dondji <wilfried.fogang@mathandpencil.com>
2025-03-04 22:37:20 -0500

    Working launch and tasks and basic task manager module

---
+++ task_manager_cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/wdond086/projects-go/task-manager-cli
+
+go 1.23.6
---
+++ task_manager_cli/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var testStr = "debugging"
+	fmt.Println("Testing vscode setup", testStr)
+}
